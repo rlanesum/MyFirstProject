7be2b77d247f3723023bbd63afbbf68a71750c82
rlanesum <mudzingwa1@tritons.iowacentral.edu>
2022-08-26 11:43:19 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,17 @@
+/******************
+	Anesu Mudzingwa
+	C++ Fall 2022
+	Due : Aug 31,2022
+	Lab 1 Introuction to C++ Programming
+	This lab is demo of my ability to work with an IDE and GitHub
+**********************/
+
+
+#include <iostream>
+
+int main() 
+{
+	std::cout << "Hello World!";
+
+	return 0;
+}
